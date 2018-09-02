0bcaa3bab28986d0f77ae6b55dc0d54c43906aa2
Adam Borek <adambor3k@gmail.com>
2018-09-02 14:20:24 +0200

    Create slugged posts :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
-  root to: 'pages#home'
+  root to: 'posts#index'
   get 'pages/about'
   get 'pages/contact'
+  resources :posts, exlude: [:show]
   resources :portfolios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902120210) do
+ActiveRecord::Schema.define(version: 20180902121154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180902120210) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
   end
 
   create_table "skills", force: :cascade do |t|
