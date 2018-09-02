6c54f73fc4e3c41ae80c739948f118916e591f0c
Adam Borek <adambor3k@gmail.com>
2018-09-02 14:29:14 +0200

    Adds status for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902121154) do
+ActiveRecord::Schema.define(version: 20180902122054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,10 @@ ActiveRecord::Schema.define(version: 20180902121154) do
   create_table "posts", force: :cascade do |t|
     t.text     "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.text     "slug"
+    t.integer  "status",     default: 0
     t.index ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
   end
 
