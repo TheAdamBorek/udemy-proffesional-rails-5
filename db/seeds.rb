871ff5dca414fe0724829f03fef74e3f29fca37c
Adam Borek <adambor3k@gmail.com>
2018-09-04 08:25:35 +0200

    Adds custom scope in portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,38 @@
 
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 
+3.times do |index|
+  Topic.create!(
+      name: "Topic #{index}"
+  )
+end
+
 9.times do |i|
-  Portfolio.create!(
+  Post.create!(
       {
           title: "Portfolio #{i}",
-          subtitle: "subtitle #{i}",
           body: lorem,
-          main_image: 'https://fakeimg.pl/600x300/',
-          thumb_image: 'https://fakeimg.pl/250x100/'
+          topic_id: Topic.all[rand 3]
       }
+  )
+end
+
+8.times do |i|
+  Portfolio.create!(
+      title: "Portfolio title: #{i}",
+      subtitle: "Ruby on Rails",
+      body: lorem,
+      main_image: 'https://fakeimg.pl/600x300/',
+      thumb_image: 'https://fakeimg.pl/250x100/'
+  )
+end
+
+1.times do |i|
+  Portfolio.create!(
+      title: "Portfolio title: #{i}",
+      subtitle: "Swift",
+      body: lorem,
+      main_image: 'https://fakeimg.pl/600x300/',
+      thumb_image: 'https://fakeimg.pl/250x100/'
   )
 end
