f931c7d7276d0143878a37e82a6d8a38bca40f71
Adam Borek <adambor3k@gmail.com>
2018-09-06 09:14:19 +0200

    Use mix-in to add placeholders for images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :main_image, :thumb_image
 
   def self.angular
@@ -7,5 +8,14 @@ class Portfolio < ApplicationRecord
 
   def self.ruby_on_rails
     where(subtitle: "Ruby on Rails")
+  end
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.main_image ||= Placeholder.generate_image width: 600, height: 300
+    self.thumb_image ||= Placeholder.generate_image width: 200, height: 100
   end
 end
