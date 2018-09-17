d54534e4727df66b5fc37c8c5d04c5af1acb930e
Adam Borek <adambor3k@gmail.com>
2018-09-17 07:51:32 +0200

    Adds devise generated files + setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :user, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root to: 'posts#index'
   get 'pages/about'
   get 'pages/contact'
