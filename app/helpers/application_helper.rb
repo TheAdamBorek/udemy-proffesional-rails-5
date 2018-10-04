11e3922ee4ca0a9b0ab9b954c7a7a8d276cadffd
Adam Borek <adambor3k@gmail.com>
2018-10-04 16:56:19 +0200

    Implement helper for source

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,28 @@ module ApplicationHelper
       link_to 'Log out', destroy_user_session_path, method: :delete
     else
       (link_to 'Log in', new_user_session_path) +
-      '<br/>'.html_safe +
-      (link_to 'Register', new_user_registration_path)
+          '<br/>'.html_safe +
+          (link_to 'Register', new_user_registration_path)
+    end
+  end
+
+  def show_source
+    unless get_source.blank?
+      greeting = "Thanks for visiting my #{get_source}"
+      content_tag :p, greeting
     end
   end
 end
+
+private
+def get_source
+  source = session[:source]
+  source.capitalize unless source.nil?
+end
+#
+#
+# <% unless get_source.blank? %>
+#   <p>Thanks for visiting my <%= get_source %></p>
+# <% end %>
+#
+#
