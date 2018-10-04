d6d5c77088992873795c4da15c36e8904e25e55d
Adam Borek <adambor3k@gmail.com>
2018-10-04 16:41:48 +0200

    Implemented view helper for authentication links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def show_login_logout_section
+    if current_user.is_authenticated
+      link_to 'Log out', destroy_user_session_path, method: :delete
+    else
+      (link_to 'Log in', new_user_session_path) +
+      '<br/>'.html_safe +
+      (link_to 'Register', new_user_registration_path)
+    end
+  end
 end
