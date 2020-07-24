93efd8dd4a00f4373ca98c413b9df7d0d9b0deb1
Adam Borek <adam.borek@babylonhealth.com>
2020-07-24 13:21:04 +0200

    Recreate DB

--- db/schema.rb
+++ db/schema.rb
@@ -49,13 +49,6 @@ ActiveRecord::Schema.define(version: 20180917053827) do
     t.index ["topic_id"], name: "index_posts_on_topic_id", using: :btree
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "percent_utilized"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
